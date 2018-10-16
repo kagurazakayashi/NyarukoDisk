e10e6e0a1418239498489255c3cd2d37ab73db44
神楽坂雅詩 <cyscxchope@gmail.com>
2018-10-16 20:49:22 +0800

    增加数据大小单位转换

--- sysinfo.php
+++ sysinfo.php
@@ -1,9 +1,10 @@
 <?php
+include_once("functions.php");
 header('Content-type:application/json');
 echo json_encode(array(
     "status" => 0,
-    "maxsize" => ini_get('post_max_size'),
-    "maxfilesize" => ini_get('upload_max_filesize'),
+    "maxsize" => unformatBytes(ini_get('post_max_size')),
+    "maxfilesize" => unformatBytes(ini_get('upload_max_filesize')),
     "timeout" => ini_get('max_execution_time')
 ));
 ?>
