cbf82f3ef839f6e0ab9f2efedcb519f16e35bc19
神楽坂雅詩 <cyscxchope@gmail.com>
2018-10-30 21:32:13 +0800

    加入示例返回值

--- exist.php
+++ exist.php
@@ -6,4 +6,48 @@ include_once("config.php");
 header('Content-type:application/json');
 $exists = new nyaExist();
 echo $exists->isexist();
+/*
+/exist.php?mode=hash&hash=c1dd19f919ac9767e939883f697ef267 返回值示例：
+{
+    "c1dd19f919ac9767e939883f697ef267": true
+}
+*/
+
+/*
+/exist.php?mode=hash-all&hash=c1dd19f919ac9767e939883f697ef267 返回值示例：
+{
+    "info": {
+        "status": 0,
+        "memory": 452272,
+        "proctime": 0.013216018676757812,
+        "files": 1
+    },
+    "files": {
+        "c1dd19f919ac9767e939883f697ef267": [
+            {
+                "fileid": "3665061501884871c133063685857e1f49f2faf9f12e1f0ee8031e6185611a51",
+                "srcname": "QQ图片20190110153341.jpg",
+                "phyname": "3665061501884871c133063685857e1f49f2faf9f12e1f0ee8031e6185611a51",
+                "ext": "jpg",
+                "dir": "2019/01/10",
+                "mime": "image/jpeg",
+                "size": "61201",
+                "uptime": "2019-01-10 11:39:51",
+                "hash": "c1dd19f919ac9767e939883f697ef267"
+            },
+            {
+                "fileid": "81f3942d77f946032cf3bb4a7c851ceed4be36d949be521d6ea8dda05bd445b2",
+                "srcname": "TIM图片20190110153341.jpg",
+                "phyname": "3665061501884871c133063685857e1f49f2faf9f12e1f0ee8031e6185611a51",
+                "ext": "jpg",
+                "dir": "2019/01/10",
+                "mime": "image/jpeg",
+                "size": "61201",
+                "uptime": "2019-01-10 11:27:52",
+                "hash": "c1dd19f919ac9767e939883f697ef267"
+            }
+        ]
+    }
+}
+*/
 ?>
--- upload.php
+++ upload.php
@@ -8,4 +8,24 @@ include_once("src/nyad_upload.php");
 header('Content-type:application/json');
 $nyaUpload = new nyaUpload;
 echo $nyaUpload->savefile();
+/*
+返回值示例：
+[
+    {
+        "status": 0,
+        "fileid": "3665061501884871c133063685857e1f49f2faf9f12e1f0ee8031e6185611a51",
+        "srcname": "QQ图片20190110153341.jpg",
+        "phyname": "3665061501884871c133063685857e1f49f2faf9f12e1f0ee8031e6185611a51.jpg",
+        "ext": "jpg",
+        "dir": "2019/01/10/",
+        "url": "http://192.168.2.100/upload/2019/01/10/3665061501884871c133063685857e1f49f2faf9f12e1f0ee8031e6185611a51.jpg",
+        "mime": "image/jpeg",
+        "size": 61201,
+        "uptime": "2019-01-10 11:39:51",
+        "md5": "c1dd19f919ac9767e939883f697ef267",
+        "memory": 518096,
+        "proctime": 0.07312202453613281
+    }
+]
+*/
 ?>
