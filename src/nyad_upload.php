c9f970d99ea80a97ff2faf6eaa72d78c3f5d22c4
神楽坂雅詩 <cyscxchope@gmail.com>
2018-10-31 20:56:06 +0800

    内容重复的文件仅在数据库虚拟创建文件

--- src/nyad_upload.php
+++ src/nyad_upload.php
@@ -2,8 +2,10 @@
 class nyaUpload {
     private $filebase = "";
     private $fileinfo = null;
+    private $postinfo = null;
     private $allarr = array();
-    private $isarray = false;
+    private $fileisarray = false;
+    private $postisarray = false;
     private $security = null;
     private $config = null;
     private $sqlconn = null;
@@ -13,7 +15,17 @@ class nyaUpload {
     function __construct() {
         if (isset($_FILES["file"])) {
             $this->fileinfo = $_FILES["file"];
-            $this->isarray = is_array($this->fileinfo["error"]);
+            if (isset($_FILES) && count($_FILES) > 0) {
+                $this->fileisarray = is_array($this->fileinfo["error"]);
+            }
+            if (isset($_POST) && count($_POST) >= 2) {
+                if (!isset($_POST["hash"]) || !isset($_POST["filename"])) return $this->fail(-103);
+                $this->postinfo = array(
+                    "hash"=>$_POST["hash"],
+                    "filename"=>$_POST["filename"]
+                );
+                $this->postisarray = is_array($this->fileinfo["filename"]);
+            }
             $this->config = new nyaUploadConfig();
             $this->security = new nyaUploadSecurity();
             $this->sqlconn = new nyaUploadDB($this->config->databaseConfig);
@@ -24,10 +36,12 @@ class nyaUpload {
     }
     /**
      * @description: 返回的错误内容
+     * @param String errid 要输出的状态码（留空则什么都不输出） 
      */
-    function fail() {
+    function fail($errid=null) {
         header("HTTP/1.1 400 Bad Request");
-        return null;
+        if ($errid) echo json_encode(array(array("status"=>$errid)));
+        die;
     }
     /**
      * @description: 从文件信息数组中取出信息，区分单个或多个文件
@@ -36,10 +50,17 @@ class nyaUpload {
      * @return String 取出的信息内容
      */
     function getfileinfo($key,$fi) {
-        if ($this->isarray) {
+        if ($this->fileisarray) {
             return $this->fileinfo[$key][$fi];
         } else {
             return $this->fileinfo[$key];
+        }
+    }
+    function getpostinfo($key,$fi) {
+        if ($this->postisarray) {
+            return $this->postinfo[$key][$fi];
+        } else {
+            return $this->postinfo[$key];
         }
     }
     /**
@@ -59,17 +80,19 @@ class nyaUpload {
         $subdir = implode("/", $nowdate).'/';
         return $subdir;
     }
-    //检查MD5是否重复，如果重复将物理文件名重新赋予，并不做移动操作
-    function chkmd5() {
-        
+
+    function optionalupload() {
+        if ($this->postinfo) {
+            
+        }
     }
     /**
      * @description: 保存文件并返回相关信息
      */
     function savefile() {
-        if (!$this->fileinfo) return $this->fail();
+        if (!$this->fileinfo && !$this->postinfo) return $this->fail();
         $filecount = 1;
-        if ($this->isarray) $filecount = count($this->fileinfo["error"]);
+        if ($this->fileisarray) $filecount = count($this->fileinfo["error"]);
         for ($fi=0; $fi < $filecount; $fi++) {
             $jtimestart = microtime(true);
             $jarr = null;
@@ -107,22 +130,41 @@ class nyaUpload {
                     "mime" => $this->getfileinfo("type",$fi),
                     "size" => $this->getfileinfo("size",$fi),
                     "uptime" => $uptimestr,
-                    "md5" => $md5
+                    "hash" => $md5
                 );
+                //查询是否可秒传
+                $dontuploadfile = false;
+                $existdata = $this->sqlconn->getFileWithHash($md5);
+                if ($existdata && !is_array($existdata)) {
+                    $jarr["status"] = -201;
+                    $jarr["error"] = $existdata;
+                } else {
+                    if (isset($existdata[0])) {
+                        $firstexistfile = $existdata[0];
+                        $jarr["phyname"] = $firstexistfile["phyname"];
+                        $jarr["ext"] = $firstexistfile["ext"];
+                        $jarr["dir"] = $firstexistfile["dir"];
+                        $jarr["mime"] = $firstexistfile["mime"];
+                        $jarr["size"] = $firstexistfile["size"];
+                        $jarr["hash"] = $firstexistfile["hash"];
+                        $jarr["status"] = 100;
+                        $dontuploadfile = true;
+                    }
+                }
                 if (file_exists($this->filebase)) {
-                    if (file_exists($this->filebase.$filename))
+                    if (!$dontuploadfile && file_exists($this->filebase.$filename))
                     {
                         $jarr["status"] = -102;
                     }
                     else
                     {
-                        if (move_uploaded_file($fromaddress, $toaddress)) {
-                            $sqlr = $this->sqlconn->insertFile($fileid,$srcfilename,$filename,$extension,$todir,$jarr["mime"],$jarr["size"],$uptimestr,$md5);
+                        if ($dontuploadfile || move_uploaded_file($fromaddress, $toaddress)) {
+                            $sqlr = $this->sqlconn->insertFile($fileid,$srcfilename,$jarr["phyname"],$jarr["ext"],$jarr["dir"],$jarr["mime"],$jarr["size"],$uptimestr,$jarr["hash"]);
                             if ($sqlr && !is_array($sqlr)) {
                                 $jarr["status"] = 200;
                                 $jarr["error"] = $sqlr;
                             } else {
-                                $jarr["status"] = 0;
+                                // $jarr["status"] = 0;
                                 if ($srcfilenamevfarr[0]) $jarr["status"] = 101;
                             }
                         } else {
